48603937b5a2de65d8b261ca61e5d275b7f6aeab
aoblea <arwinoblea@gmail.com>
2020-11-19 22:48:12 -0500

    Initial Commit

---
+++ AnimeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimeApp
+//
+//  Created by Arwin Oblea on 11/19/20.
+//  Copyright © 2020 iarwinscott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
